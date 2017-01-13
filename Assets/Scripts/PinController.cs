00d1395117d6080888c240ebbfabfa874adc28d6
Martichoras <emil@rhoeg.dk>
2017-01-13 01:05:12 +0100

    Version 1.0 beta (update 1)
    - minor tweaks
    - added wait message
    - increased GUI quality
    - added new scene for settings

--- Assets/Scripts/PinController.cs
+++ Assets/Scripts/PinController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class PinController : MonoBehaviour {
 
-	private int[] angle = new int[12] {15,20,30,60,70,75,-15,-20,-30,-60,-70,-75};
+	private int[] angle = new int[12] {15,20,30,60,70,75,-15,-20,-30,-60,-70,-75}; // predetermined array of possible angles 
 	private int random;
 	private int localAngle;
 	//private Vector3 rot;
@@ -13,26 +13,24 @@ public class PinController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		
-		random = Random.Range(0,12);
-		localAngle = angle[random];
+		random = Random.Range(0,12); // picks a random angle for this specific clone
+		localAngle = angle[random];  // picks a random angle for this specific clone
 
-		transform.LookAt(Vector3.zero);
+		transform.LookAt(Vector3.zero); // turn pin towards origo
 
-		transform.Rotate(0, 0, localAngle);
-
-	}
-	
-	// Update is called once per frame
-	void Update () {
+		transform.Rotate(0, 0, localAngle); // rotates pin to the selected z-angle
 
 	}
 
 	void OnCollisionEnter(Collision col){
-		
-		if(col.gameObject.tag == "Pin"){
-			Destroy(gameObject);
+
+		// This function is primarily to avoid overlaying pins by destroying this pin if it collides with another pin.
+		// PinSpawner.cs makes sure pins keep spawning like this untill there's a total of e.g. 400 pins in the scene.
+
+		if(col.gameObject.tag == "Pin"){ //if colliding object is of type 'Pin'
+			Destroy(gameObject); //destroy this game object.
 			//Debug.Log("Object destroyed!");
-		} else if(col.gameObject.tag == "PipPopPin"){
+		} else if(col.gameObject.tag == "PipPopPin"){ // makes sure PipPopPin destroys this object. could be an || statement, but meh. I like spaghetti.
 			Destroy(this.gameObject);
 			Debug.Log("Pip-Pop Pin destroyed other pin!");
 		}
---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Rotate : MonoBehaviour 
+{
+	void Update() 
+	{
+		// Rotate the object around its local X axis at 1 degree per second
+		transform.Rotate(-Vector3.forward * Time.deltaTime*50.0f);
+
+		// ...also rotate around the World's Y axis
+		//transform.Rotate(Vector3.out * Time.deltaTime*10.0f, Space.World);
+	}
+}
